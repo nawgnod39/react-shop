f9101c3ecfdf3cfad2e0fbd1d2b0e1d0e1b4a63a
takdongwan <dongwantak@gmail.com>
2021-03-03 23:59:06 +0900

    Uploadpage handler 추가

--- client/src/components/views/UploadProductPage/UploadProductPage.js
+++ client/src/components/views/UploadProductPage/UploadProductPage.js
@@ -1,9 +1,59 @@
-import React from 'react'
+import React,{useState} from 'react'
+import {Typograpy, Button, Form, Input } from 'antd';
 
+
+//const {Title} = Typograpy;
+const {TextArea} = Input;
 function UploadProductPage() {
+
+    const [Title, setTitle] = useState("")
+    const [Description, setDescription] = useState("")
+    const [Price, setPrice] = useState(0)//가격이라 0부터 시작 
+    const [Contienet, setContienet] = useState(1)
+
+    const [Image, setImage] = useState([])
+    const titleChangeHandler = (event)=>{
+        setTitle(event.currentTarget.value)
+    }
+    const descriptionChangeHandler = (event)=>{
+        setDescription(event.currentTarget.value)
+    }
+    const priceChangeHandler = (event)=>{
+        setPrice(event.currentTarget.value)
+    }
     return (
-        <div>
-            UploadProductPage
+        <div style={{ maxWidth: '700px', magin: '2rem auto' }}>
+            <div style={{ textAlign: 'center', marginBottom: '2rem' }}>
+                <h2 level={2}> 여행 상품 업로드 </h2>
+                {/*level 은 1 이 제일 큰size*/} 
+
+            </div>
+
+            <Form>
+                {/*Drop zone */}
+                <br />
+                <br />
+                <label>이름</label>
+                <Input onChange={titleChangeHandler} value={Title}  />
+                <br />
+                <br />
+                <label>설명</label>
+                <TextArea onChange={descriptionChangeHandler} value={Description}/>
+                <br />
+                <br />
+                <label>가격($)</label>
+                <Input type="number" onChange={priceChangeHandler} vaule ={Price}/>
+                <br />
+                <br />
+                <select>
+                        <option></option>
+                </select>
+                <br />
+                <br />
+                <Button>
+                    확인 
+                </Button>
+            </Form>
         </div>
     )
 }
