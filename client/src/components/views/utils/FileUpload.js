3653792eb6511e0a08adcc68d71a048a5c9762bd
takdongwan <dongwantak@gmail.com>
2021-03-05 04:16:10 +0900

    prodcut.js 생성

--- client/src/components/views/utils/FileUpload.js
+++ client/src/components/views/utils/FileUpload.js
@@ -1,11 +1,43 @@
 import React from 'react'
 import Dropzone from 'react-dropzone'
 import {Icon} from 'antd';
+import axios from 'axios';
 
+//frontend 에서 파일을back end 에 전달을하고 backend 에서는 파일을 저장한다 이를위해 multer이 필요하며  파일저장정보를 front end 로 전달해준다 .
+//전해주기위해서 axios 사용
 function FileUpload() {
+   
+   
+    const dropHandler=(files)=>{
+        
+        let formData = new FormDate();
+        const config = {
+
+            header:{'content-type':'multipart/form-data'}
+        }
+    
+        formData.append("file",files[0])
+
+        axios.post('/api/product/image',formData,config)//fort에서만 할것들 처리 
+        .ten(reasponse=>{
+            if(response.data.success){
+
+            }else{
+                alert("파일을 저장하는데 실패했습니다.")
+                    
+            }
+
+        })
+        //formData와 config를 같이 넣어주지않으면 오류가 발생 
+        }
+    }
+
+
+
+
     return (
         <div style ={{display: 'flex', justifyContent:'space-between'}}>
-            <Dropzone onDrop={acceptedFiles => console.log(acceptedFiles)}>
+            <Dropzone onDrop={dropHandler}>
             {({getRootProps, getInputProps}) => (
                 <section>
                     style={{
@@ -21,6 +53,6 @@ function FileUpload() {
             </Dropzone>
         </div>
     )
-}
+
 
 export default FileUpload
---
+++ server/routes/product.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+
+
+//=================================
+//            Product
+//=================================
+
+router.post('/image',(req,res)=>{
+    //가져온 이미지를 저장을 해주면 된다.
+    //multer 사용
+
+
+
+})//이미 index 에서 api product를 거치고 product.js로 온것.
+//fileupload 에서 보내는 request를   product.js에서 받아줌.
+ 
+
+module.exports = router;
